b14c8c1d4e49c45ab2876d9a7243fc8f0d6e316c
Milos <m.miljevic@hotmail.com>
2024-11-04 13:11:45 +0100

    more js for pets area

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ async function petsArea() {
     clone.querySelector('h3').textContent = pet.name;
     clone.querySelector('.pet-description').textContent = pet.description;
     clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear);
+    if (!pet.photo) pet.photo = 'images/fallback.jpg';
     clone.querySelector('.pet-card-photo img').src = pet.photo;
     clone.querySelector(
       '.pet-card-photo img'
@@ -43,4 +44,17 @@ function createAgeText(birthYear) {
   if (age == 0) return 'Less than a year old';
 
   return `${age} years old`;
+}
+
+//pet filter button code
+
+const allButtons = document.querySelectorAll('.pet-filter button');
+
+allButtons.forEach(el => {
+  el.addEventListener('click', handleButtonClick);
+});
+
+function handleButtonClick(e) {
+  allButtons.forEach(el => el.classList.remove('active'));
+  e.target.classList.add('active');
 }
